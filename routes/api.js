ea24ea3f50e547efea6625b382a89023722b4cdd
Glitch (zgleman-issuetrack) <none>
2019-10-15 12:42:12 +0000

    🏮🎩 Checkpoint ./routes/api.js:7037687/1055

--- routes/api.js
+++ routes/api.js
@@ -14,7 +14,17 @@ var ObjectId = require('mongodb').ObjectID;
 var mongoose = require('mongoose');
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 mongoose.connect(CONNECTION_STRING, {useNewUrlParser: true});
-
+const Issue = mongoose.model('Issue', {
+  _id:	String,
+issue_title:	String,
+issue_text: String,
+created_on:	Date,
+updated_on:	Date,
+created_by:	String,
+assigned_to:	String,
+open:	Boolean,
+status_text:	String
+});
 
 module.exports = function (app) {
 
@@ -27,6 +37,16 @@ module.exports = function (app) {
     
     .post(function (req, res){
       var project = req.params.project;
+    const incoming = new Issue({
+      issue_title:	req.params.Title,
+      issue_text: req.params.Text,
+      created_on:	new Date(),
+      created_by:	req.params.Created_by || "",
+      assigned_to:	String,
+      open:	Boolean,
+      status_text:	String
+      
+    })
       
     })
     
