f475be5344cf998883d77096eb442b0a037004ab
Glitch (zgleman-issuetrack) <none>
2019-10-15 12:03:06 +0000

    ❤️🚃 Checkpoint ./server.js:7037687/78

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
-
+var helmet = require('helmet');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
@@ -15,7 +15,8 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-
+app.use(helmet());
+app.use(helmet.xssFilter());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
