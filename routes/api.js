00897e81f9986eda286010eb0ec04230f62fc86d
Glitch (zgleman-issuetrack) <none>
2019-10-17 10:41:53 +0000

    👷👣 Checkpoint ./routes/api.js:7037687/196 ./tests/2_functional-tests.js:7037687/4

--- routes/api.js
+++ routes/api.js
@@ -15,7 +15,7 @@ var mongoose = require('mongoose');
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 mongoose.connect(CONNECTION_STRING, {useNewUrlParser: true});
 const Issue = mongoose.model('Issue', {
-  _id: String,
+ 
 issue_title:	String,
 issue_text: String,
 created_on:	Date,
@@ -41,7 +41,6 @@ module.exports = function (app) {
     .post(function (req, res){
       var project = req.params.project;
       Issue.create({
-      _id: ObjectId,
       issue_title:	req.body.issue_title,
       issue_text: req.body.issue_text,
       created_on:	new Date(),
@@ -50,7 +49,10 @@ module.exports = function (app) {
       assigned_to:	req.body.assigned_to || "",
       open:	true,
       status_text:	req.body.status_text || ""
-      }, function(err));
+      }, function(err, data){
+        if (err) return res.send({error:'Error saving to database'});
+        res.send(data);
+      });
   
     
       
